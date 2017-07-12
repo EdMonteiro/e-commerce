e0b6cd961e594577c1c4cf94bcb3c832905806ae
Ed Monteiro <edemirmonteir@gmail.com>
2017-07-12 16:52:05 +0100

    preparing for jquery plugin

--- public/js/t-shirt.color.js
+++ public/js/t-shirt.color.js
@@ -1,6 +1,7 @@
 // test jquery change t-shirt color on click
 $(document).ready(function() {
 
+    // t-shirt colors to choose from
     $(".white").click(function(){
         $("#t-shirt").css("background-color", "white");
     })
@@ -13,4 +14,17 @@ $(document).ready(function() {
     $(".navy").click(function(){
         $("#t-shirt").css("background-color", "navy");
     })
+
 });
+    // image to add to personalizerd t-shirt
+    // document.getElementById("input-image").onSubmit(function () {
+
+    //     document.getElementById("inner-img").css("");
+    // });
+
+        document.getElementById("input-image").addEventListener("submit", myFunction);
+
+        function myFunction() {
+            alert("The form was submitted");
+        }
+
